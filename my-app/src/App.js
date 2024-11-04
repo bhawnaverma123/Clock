ffa6a424bf8c66d394dff2c499a7ceecc63e7950
@bhawnaverma123 <vermab2974@gmail.com>
2024-11-04 15:01:36 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DigitalClock from './digitalclock'; // Adjust the path as necessary
+import './digitalclock.css'; 
+
+const App = () => {
+  return (
+    <div>
+      <DigitalClock />
+    </div>
+  );
+};
+
+export default App;
